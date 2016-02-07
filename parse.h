0d1feb1172a7c86954389830bb719c7b32919ef1
Christopher Pribyl <cprib001@ucr.edu>
2016-02-06 19:21:15 -0800

    parse() now seperates semicolons from user input

--- parse.h
+++ parse.h
@@ -1,13 +1,22 @@
+#include<iostream>
 #include<vector>
 #include<string>
 #include<boost/tokenizer.hpp>
 #include<boost/algorithm/string.hpp>
 
-using namespace std;
-using namespace boost;
+std::vector<std::string> parse(std::string text) {
+  
+  std::vector<std::string> tokenList;
 
-vector<string> parse(string text) {
-  vector<string> tokenList;
-  split(tokenList, text, is_any_of(" "), token_compress_on);
+  split(tokenList, text, boost::is_any_of(" "), boost::token_compress_on);
+
+  int tokenListSize = tokenList.size();
+  for(int i = 0; i < tokenList.size(); i++) {
+    if(tokenList.at(i).at(tokenList.at(i).size() - 1) == ';' && tokenList.at(i).size() != 1) {
+      tokenList.at(i).erase(tokenList.at(i).size() - 1, 1);
+      tokenList.insert(tokenList.begin() + i + 1, ";");
+    }
+  }
+
   return tokenList;
 }
