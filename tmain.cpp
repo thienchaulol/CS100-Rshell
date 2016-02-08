26d7980f9176c861619e7b4d91de3aa0a33d50c4
Christopher Pribyl <cprib001@ucr.edu>
2016-02-08 03:03:18 -0800

    parsing now works

--- tmain.cpp
+++ tmain.cpp
@@ -42,7 +42,7 @@ void execute(char **argv) {
 }
 
 vector<pair<char, bool> > hashAndSlash(string str) {
-  vector<pair<char, bool> > specVec;
+  vector<pair<char, bool> > v;
   bool isInWord = false;
   bool isAfterBackslash = false;
 
@@ -53,20 +53,73 @@ vector<pair<char, bool> > hashAndSlash(string str) {
     }
     else if(str.at(i) != ' ' && str.at(i) != '#') {
       isInWord = true;
-      if(str.at(i) == '\\') {
+      if(str.at(i) == '\\' && !isAfterBackslash) {
+	v.push_back(pair<char, bool>(str.at(i), isAfterBackslash));
 	isAfterBackslash = true;
-	specVec.push_back(pair<char, bool>(str.at(i), isAfterBackslash));
 	if(i + 1 <= str.size() - 1)
 	  i++;
       }
     }
     else if(str.at(i) == '#' && !isInWord)
       break;
-    specVec.push_back(pair<char, bool>(str.at(i), isAfterBackslash));
+    v.push_back(pair<char, bool>(str.at(i), isAfterBackslash));
     isAfterBackslash = false;
   }
 
-  return specVec;
+  return v;
+}
+
+vector<pair<string, bool> > parse(vector<pair<char, bool> > v) {
+  vector<pair<string, bool> > s;
+  
+  while(!v.empty()){
+    string command;
+    string connector;
+    
+    while(v.at(0).first == ' ')
+      v.erase(v.begin());
+
+    int i = 0;
+    for(i = 0; i < v.size(); i++) {
+      if(v.at(i).first == '&' && v.at(i).second == false)
+	break;
+      else if(v.at(i).first == '|' && v.at(i).second == false)
+	break;
+      else if(v.at(i).first == ';' && v.at(i).second == false)
+	break;
+    }
+
+    for(int j = 0; j < i; j++) {
+      if(v.at(0).first == '\\' && v.at(0).second == false)
+	v.erase(v.begin());
+      else {
+	command += v.at(0).first;
+	v.erase(v.begin());
+      }
+    }
+
+    if(!v.empty()) {
+      if(v.at(0).first == '&' || v.at(0).first == '|') {
+	connector += v.at(0).first;
+	v.erase(v.begin());
+      }
+      if(v.at(0).first == '&' || v.at(0).first == '|') {
+	connector += v.at(0).first;
+	v.erase(v.begin());
+      }
+      else if(v.at(0).first == ';') {
+	connector += v.at(0).first;
+	v.erase(v.begin());
+      }
+    }
+
+    if(!command.empty()) 
+      s.push_back(pair<string, bool>(command, false));
+    if(!connector.empty())
+      s.push_back(pair<string, bool>(connector, true));
+  }
+
+  return s;
 }
 
 int main() {
@@ -80,13 +133,22 @@ int main() {
     char *argv[line.size() + 1];
     vector<pair<char, bool> > specVec = hashAndSlash(line);
 
+    cout << "specVec.first and specVec.second:" << endl;
     for(int i = 0; i < specVec.size(); i++)
       cout << specVec.at(i).first;
     cout << endl;
-
     for(int i = 0; i < specVec.size(); i++)
       cout<< specVec.at(i).second;
+    cout << endl << endl;
+
+    vector<pair<string, bool> > parsedVec = parse(specVec);
+
+    cout << "parsedVec.second and parsedVec.first:" << endl;
+    for(int i = 0; i < parsedVec.size(); i++) 
+      cout << parsedVec.at(i).second << ' ' << parsedVec.at(i).first << endl;
     cout << endl;
+
+    cout << "output:" << endl;
 
     char *c = new char[line.size() + 1];
     copy(line.begin(), line.end(), c);
