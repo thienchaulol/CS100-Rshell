7abb8a97a5f781f545275ecaa47587424e1ad75e
Thien Chau <tchau006@hammer.cs.ucr.edu>
2016-02-11 22:50:00 -0800

    found bug, added some test cases

---
+++ tests/exit.sh
@@ -0,0 +1,1 @@
+#tests exit and commands with exit
--- tests/single_command.sh
+++ tests/single_command.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
+#change directory to bin and run rshell executable
+#all text between EOF will be executed by rshell
+../bin/./rshell << EOF
 ls
-
 ls -a
-
 ls -al
+date
+cal
+invalidCommand
+git status
+echo A
+time
+alias
+uptime
+who
+exit
+EOF
+printf "\n"
